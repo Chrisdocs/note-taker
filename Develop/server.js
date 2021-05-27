cc1fda9672d27b9a5c7dbcc5cfbf1ab4960cef7d
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-05-27 16:13:03 -0500

    get notes and index html pages

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,9 @@
+// dependecies
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
-
+// create express server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,46 +14,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// GET /notes should return the notes.html file.
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
+// GET * should return the index.html file.
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/assets/js/index.js'))
-});
-
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-let notes;
-
-app.post('/api/notes', (req, res) => {
-    var addNote = req.bodWhen(path.join(__dirname, './db/db.json', 'utf8')
-    .then( function (data) {
-        notes = JSON.parse(data);
-        if (notes.id || notes.id === 0) {
-            let currentNote = notes[addNote.id];
-            currentNote.title = addNote.title;
-            currentNote.text = addNote.text;
-        } else {
-            notes.push(addNote);
-        }
-        fs.writeFileWhen(path.join(__dirname, './db/db.json'), JSON.stringify(notes))
-        .then(function () {
-            console.log('added note!')
-        })
-    })
-    )
-    res.json(addNote);
-});
-
-
-
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
